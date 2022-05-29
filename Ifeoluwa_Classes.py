166fdd34f3a6e0cb5742043350cd74b2dcfabea6
Ifeoluwa Ogungbemi <79316168+ifemiii@users.noreply.github.com>
2022-05-30 00:31:32 +0100

    Update Ifeoluwa_Classes.py

--- Ifeoluwa_Classes.py
+++ Ifeoluwa_Classes.py
@@ -1,11 +1,22 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
     def __init__(self, name, age, tracks, score):
-        self.name=name
-        self.age=age
-        self.tracks=tracks
-        self.score=score
-    
+        self.name=str(name)
+        self.age=int(age)
+        self.tracks=list(tracks)
+        self.score=float(score)
+
+    def change_name(self, new_name):
+        self.name=new_name
+        print("Name: ", self.name)
+    def change_age(self, new_age):
+        self.age=new_age
+        print("Age: ", self.age)
+    def add_track(self, new_track):
+        self.tracks.append(new_track)
+        print("Tracks: ", self.tracks)
+    def get_score(self):
+        return self.score
 
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
